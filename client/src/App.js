7252a56b20288eb8e33f35382bb0a2504772d202
davidruvolo51 <davidruvolo51@gmail.com>
2020-11-18 20:11:38 +0100

    updated dependencies, added webpack, rewrote app

--- client/src/App.js
+++ client/src/App.js
@@ -2,89 +2,86 @@
 // FILE: App.js
 // AUTHOR: David Ruvolo
 // CREATED: 2019-11-01
-// MODIFIED: 2019-11-01
+// MODIFIED: 2020-11-18
 // PURPOSE: build ui
 // DEPENDENCIES: react
 // STATUS: working
 // COMMENTS: NA
 ////////////////////////////////////////////////////////////////////////////////
-// BEGIN
-import React, { Component } from 'react';
-class App extends Component {
+import React, { useState } from 'react';
+function App() {
 
 	// set state
-	state = {
-		inputValue: 1,
-		inputPower: 2,
-		sqrdJS: 0,
-		sqrdR: 0,
-	};
+	const [inputValue, setInputValue] = useState(1);
+	const [inputPower, setInputPower] = useState(2);
+	const [squaredJS, setSquaredJS] = useState(0);
+	const [squaredR, setSquaredR] = useState(0);
 
 	// on form submit function
 	// send input values to /api/data/ and wait for response
-	handleSubmit = async e => {
+	const handleSubmit = async e => {
 		e.preventDefault();
 
 		// create response through POST
-		const response = await fetch("/data", {
+		const response = await fetch("http://localhost:5000/data", {
 			method: "POST",
+			mode: "cors",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ value: this.state.inputValue, power: this.state.inputPower }),
+			body: JSON.stringify({ value: inputValue, power: inputPower }),
 		});
+		
 		const body = await response.json();
 
 		// update state
-		this.setState({ sqrdJS: body.sqrdJS, sqrdR: body.sqrdR });
+		setSquaredJS(body.squaredJS);
+		setSquaredR(body.squaredR);
 	};
 
 	////////////////////////////////////////
 	// RENDER APP
-	render() {
-		return (
-			<>
-				<header className="header">
-					<p>R &#10084; React</p>
-				</header>
-				<main className="main" id="main" aria-label="main content">
-					<section className="section">
-						<h1>Example app using R, React, and Node.js</h1>
-						<p>In this example, we are sending a number the server, squaring by a number of our choice using R and JavaScript, and then sending it back to our react component.</p>
-						<form className="form" onSubmit={this.handleSubmit}>
-							<legend>Define Values</legend>
-							<fieldset>
-								<label htmlFor="num">Enter a number</label>
-								<span>Between 1 and 1000</span>
-								<input id="num" type="number" value={this.state.inputValue} min="1" max="1000"
-									onChange={e => this.setState({ inputValue: e.target.value })} />
-							</fieldset>
-							<fieldset>
-								<label htmlFor="power">Enter an exponent</label>
-								<span>Between 2 and 10</span>
-								<input id="power" type="number" value={this.state.inputPower} min="2" max="10" onChange={e => this.setState({ inputPower: e.target.value })} />
-							</fieldset>
-							<button type="submit">Submit</button>
-						</form>
-						<h3>Results</h3>
-						{
-							// render only after first entry
-							this.state.sqrdJS > 0
-								? (
-									<>
-										<label>via JavaScript using <code>Math.pow(...)</code></label>
-										<output>{this.state.sqrdJS}</output>
-										<label>via R using <code>sqrt()</code></label>
-										<output>{this.state.sqrdR}</output>
-									</>
-								)
-								: <output>Results will appear here</output>
-						}
-					</section>
-				</main>
-			</>
-		);
-	}
+	return (
+		<>
+			<header className="header">
+				<p>R &#10084; React</p>
+			</header>
+			<main className="main" id="main" aria-label="main content">
+				<section className="section">
+					<h1>Example app using R, React, and Node.js</h1>
+					<p>In this example, we are sending a number the server, squaring by a number of our choice using R and JavaScript, and then sending it back to our react component.</p>
+					<form className="form" onSubmit={(e) => e.preventDefault()}>
+						<legend>Define Values</legend>
+						<fieldset>
+							<label htmlFor="num">Enter a number</label>
+							<span>Between 1 and 1000</span>
+							<input id="num" type="number" min="1" max="1000" onChange={(e) => setInputValue(e.target.value)} />
+						</fieldset>
+						<fieldset>
+							<label htmlFor="power">Enter an exponent</label>
+							<span>Between 2 and 10</span>
+							<input id="power" type="number" min="2" max="10" onChange={(e) => setInputPower(e.target.value)} />
+						</fieldset>
+						<button type="submit" onClick={(e) => handleSubmit(e)}>Submit</button>
+					</form>
+					<h3>Results</h3>
+					{
+						// render only after first entry
+						squaredJS > 0
+							? (
+								<>
+									<label>via JavaScript using <code>Math.pow(...)</code></label>
+									<output>{squaredJS}</output>
+									<label>via R using <code>squared()</code></label>
+									<output>{squaredR}</output>
+								</>
+							)
+							: <output>Results will appear here</output>
+					}
+				</section>
+			</main>
+		</>
+	);
 }
 
 export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,80 @@
+////////////////////////////////////////////////////////////////////////////////
+// FILE: webpack.config.js
+// AUTHOR: David Ruvolo
+// CREATED: 2020-09-26
+// MODIFIED: 2020-11-18
+// PURPOSE: loads dev or prod configuration based on script param `env`
+// DEPENDENCIES: see common
+// STATUS: working
+// COMMENTS: NA
+////////////////////////////////////////////////////////////////////////////////
+
+// load
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+console.log(path.__dirname)
+
+// configuration
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "public/"),
+        filename: "r_react_demo.js",
+    },
+    devServer: {
+        port: 8000,
+        publicPath: "/",
+        hot: true,
+        writeToDisk: true,
+    },
+    plugins: [
+        // new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "r_react_demo.css",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(s[ac]ss|css)$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    "file-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    "file-loader"
+                ]
+            },
+        ]
+    },
+    resolve: {
+        extensions: [
+            ".js",
+            ".jsx"
+        ]
+    }
+}
